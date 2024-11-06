c5af2f4ead8d0d1f8940228a916fd6c2f30e034a
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-11-06 18:11:48 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (7, False, 2.5, "Hello")
+print(immutable_var)
+#immutable_var[0] = 18 # Кортеж неизменяемая коллекция
+
+mutable_list = [23, 9.3, "Lists", True]
+mutable_list[1] = 6.66
+mutable_list += [444]
+print(mutable_list)
+
